6236ac2587a91496589de58d21d99c8eb7eaa0b4
undefined <oncwnuDXBfF1L58V880rCDrOyO7U@git.weixin.qq.com>
2019-11-26 10:53:31 +0800

    feat: 添加构建分析和解决antd icon文件太大

--- config-overrides.js
+++ config-overrides.js
@@ -2,20 +2,21 @@ const {
   override,
   addLessLoader,
   fixBabelImports,
+  addWebpackAlias,
   addDecoratorsLegacy,
   addBundleVisualizer,
 } = require('customize-cra')
+const path = require('path')
 
 module.exports = override(
-  // 使用eslint 配置
-  // useEslintRc("./.eslintrc.js"),
-
   // antd 按需加载
   fixBabelImports('import', {
     libraryName: 'antd',
     libraryDirectory: 'es',
     style: true,
   }),
+
+  addBundleVisualizer({}),
 
   // less 支持配置
   addLessLoader({
@@ -29,10 +30,13 @@ module.exports = override(
   // add webpack bundle visualizer if BUNDLE_VISUALIZE flag is enabled
   process.env.BUNDLE_VISUALIZE == 1 && addBundleVisualizer(),
 
-  // add an alias for "ag-grid-react" imports
-  // addWebpackAlias({
-  //   ["ag-grid-react$"]: path.resolve(__dirname, "src/shared/agGridWrapper.js")
-  // }),
+  // 添加 webpack 别名，这里用于解决 icon 文件太大的问题
+  addWebpackAlias({
+    '@ant-design/icons/lib/dist$': path.resolve(
+      __dirname,
+      './src/config/icons.js',
+    ),
+  }),
 
   // adjust the underlying workbox
   // adjustWorkbox(wb =>
